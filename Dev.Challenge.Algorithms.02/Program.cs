a302defe857e19c6800c45c6efa15e433757419b
PGonzalezDev <pgonzalezdeveloper@gmail.com>
2022-03-17 16:08:41 -0300

    Add project files.

---
+++ Dev.Challenge.Algorithms.02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Dev.Challenge.Algorithms._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una contraseña: ");
+            string password = Console.ReadLine();
+            Console.Clear();
+
+            int i = 0;
+            bool successfulPassEntry = false;
+
+            while (i < 3)
+            {
+                Console.WriteLine("Ingrese nuevamente su contraseña: ");
+                string repeatedPassword = Console.ReadLine();
+
+                if (password == repeatedPassword)
+                {
+                    Console.WriteLine("Felicitaciones, recordás tu contraseña");
+                    successfulPassEntry = true;
+                    break;
+                }
+
+                i++;
+            }
+
+            if (!successfulPassEntry)
+            {
+                Console.WriteLine("Tenes que ejercitar la memoria");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Dev.Challenge.Algorithms/program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Dev.Challenge.Algorithms._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Ingrese un numero entero: ");
+            string response = Console.ReadLine();
+            number = int.Parse(response);
+
+            bool isPrimeNumber = true;
+
+            for (int i = 2; i <= number - 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrimeNumber = false;
+                }
+            }
+
+            string result;
+
+            if (number != 1 && isPrimeNumber)
+            {
+                result = " es primo";
+            }
+            else
+            {
+                result = " no es primo";
+            }
+
+            Console.WriteLine("El numero " + number + result);
+        }
+    }
+}
